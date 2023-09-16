0fbbd15ad3931b696b0a67ca59de0178a7b207dc
DigitalSocrates <user@DigitalSocrates.com>
2023-09-16 12:48:00 -0700

    downgraded python vesion to 3.8

--- Web_Scrapping_Experiments/src/utils.py
+++ Web_Scrapping_Experiments/src/utils.py
@@ -1,11 +1,12 @@
 """ utility file mostly for convinience """
 import os
-import requests
-import pandas as pd
 from itertools import permutations
 from datetime import datetime
+import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 from src.game import Game
+
 
 def scrape_lottery_data(year : int, file_path : str, date_format : str, save_as : str = 'parquet'):
     """ scrape lottery web site by year
@@ -19,10 +20,15 @@ def scrape_lottery_data(year : int, file_path : str, date_format : str, save_as 
     games_df = game.get_game_dataframe()
 
     # URL for Powerball past drawings
-    url = f"https://www.walottery.com/WinningNumbers/PastDrawings.aspx?gamename=powerball&unittype=year&unitcount={year}"
+    url = "https://www.walottery.com/WinningNumbers/PastDrawings.aspx?" +\
+            f"gamename=powerball&unittype=year&unitcount={year}"
 
     # Send an HTTP GET request
-    response = requests.get(url)
+    try:
+        response = requests.get(url, timeout=60) # will wait for 60 seconds
+    except requests.exceptions.Timeout:
+        print(f'was not able to get url {url} due to timeout')
+        return
 
     # Parse the HTML content of the page
     soup = BeautifulSoup(response.text, "html.parser")
@@ -35,8 +41,8 @@ def scrape_lottery_data(year : int, file_path : str, date_format : str, save_as 
         # set date of the game
         game.set_game_date(datetime.strptime(numbers.find('thead').h2.text.strip(), date_format))
         # set balls
-        td = numbers.find('td', class_='game-balls')
-        game.set_numbers(td.find('ul').find_all('li'))
+        td_cell = numbers.find('td', class_='game-balls')
+        game.set_numbers(td_cell.find('ul').find_all('li'))
         # add game results to dataframe
         games_df = pd.concat([games_df, pd.DataFrame.from_records([game.get_df_row()])])
 
@@ -63,13 +69,13 @@ def read_all_parquet_files(directory : str):
     dfs = []
     for parquet_file in parquet_files:
         file_path = os.path.join(directory, parquet_file)
-        df = pd.read_parquet(file_path)
-        dfs.append(df)
+        df_temp = pd.read_parquet(file_path)
+        dfs.append(df_temp)
 
     if dfs:
         return pd.concat(dfs, ignore_index=True)
-    else:
-        return None
+
+    return None
 
 
 def generate_value_permutations(values = ('p', 'q', 'd')):
